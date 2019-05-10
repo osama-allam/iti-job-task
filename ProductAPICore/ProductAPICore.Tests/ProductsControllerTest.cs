978bf65b2fac876e954faca3990812b644692552
Osama <osama_allam@yamil.com>
2019-05-10 21:59:39 +0200

    - Remove  sslPort to disable https - Remove unused pages - Create Test Project

---
+++ ProductAPICore/ProductAPICore.Tests/ProductsControllerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace ProductAPICore.Tests
+{
+    public class ProductsControllerTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+        [Test]
+        public void GetProducts_WhenCalled_ReturnsOkResult()
+        {
+        }
+    }
+}
